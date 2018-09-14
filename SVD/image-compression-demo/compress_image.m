2c2556146a77be68d2338e39c7757204e388beca
Jernej Vivod <je.vivod@gmail.com>
2018-09-14 11:25:31 +0200

    Modify reconstruction matrix computations and add cummulative sum of singular values computation

--- SVD/image-compression-demo/compress_image.m
+++ SVD/image-compression-demo/compress_image.m
@@ -1,25 +1,30 @@
-% compress_image(image_name, num_singular_values)
+% function [M1, M2, compression_ratio, cumsum_sv] = compress_image(image_name, num_singular_values)
 %
 % Compress image with name image_name using singular value decomposition by
 % keeping only the number of singular values specified by num_singular_values argument. 
 % The function returns the two matrices needed to reconstruct the original
-% image and the compression ratio achieved (ratio of number of elements in
-% matrices).
+% image, the compression ratio achieved (ratio of number of elements in
+% matrices) and a row vector of the comulative sum of the singular values.
 %
 % Author: Jernej Vivod
 %
-function [M1, M2, compression_ratio] = compress_image(image_name, num_singular_values)
+function [M1, M2, compression_ratio, cumsum_sv] = compress_image(image_name, num_singular_values)
 	im = imread(image_name);	% Read image.
 	im = im(:,:,1);				% Remove third dimension.
 	im = double(im);
 	[U E V] = svd(im);			% Perform SVD.
 	% Approximate original image with a lower rank matrix.
-	M1 = U * E(:,1:num_singular_values);	% Matrices needed to reconstruct the image
+	% Matrices needed to reconstruct the image:
+	M1 = U(:, 1:num_singular_values) * E(1:num_singular_values, 1:num_singular_values);	
 	M2 = V(:,1:num_singular_values)';
 	
 	% Compute compression ratio.
 	original_size = numel(im);
 	compressed_size = numel(M1) + numel(M2);
 	compression_ratio = compressed_size / original_size;
+	
+	% Compute cummulative sum of the singular values.
+	diag_elements = diag(E);
+	cumsum_sv = cumsum(diag_elements)';
 	
 end
